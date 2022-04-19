2b8d683618f8fea9921b88f9de8a5413ed2bc7ab
Gerald <geraldckj13@gmail.com>
2022-04-19 19:45:43 +0800

    some edits

---
+++ src/Pages/Admin/CreateNewEvent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>test</p>
+        <create-event-form></create-event-form>
+    </div>
+</template>
+
+<script>
+import CreateEventForm from '../../Components/Admin/CreateEventForm.vue'
+
+export default ({
+    components:{
+        CreateEventForm
+    },
+    setup() {
+        
+    
+    },
+})
+</script>
